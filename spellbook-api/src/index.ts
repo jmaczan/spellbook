35ee5ed762bb41156bb7b38e2b8316acb60fc5be
jmaczan <jedrzejpawel@maczan.pl>
2022-03-02 22:15:03 +0100

    Extract registry

--- spellbook-api/src/index.ts
+++ spellbook-api/src/index.ts
@@ -1,3 +1,18 @@
-const variable: string = 'some';
+import express from 'express';
+import dotenv from 'dotenv';
 
-console.log(variable);
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("What's up doc ?!");
+});
+
+app.get('/api/search', (req, res) => {
+    res.send(req.query.query);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`server running : http://${process.env.HOST}:${process.env.PORT}`);
+});
