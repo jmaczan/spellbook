e367796c7f8a45f9485078d350bef654c4e19a2e
jmaczan <jedrzejpawel@maczan.pl>
2022-03-05 14:30:08 +0100

    Add howto to home page

--- web/src/components/home/home.tsx
+++ web/src/components/home/home.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './home.scss';
 
 export const Home = () => (
   <section>
-    <div>The magic is happening here ✨🔮</div>
+    <div className='home__howto'>
+      <div className='howto__step'>
+        <Link to='/install'>Install</Link>&nbsp;Spellbook ➡️&nbsp;
+      </div>
+      <div className='howto__step'>
+        <Link to='/search'>Find</Link>&nbsp;your favourite script&nbsp;
+      </div>
+      <div className='howto__step'>
+        ➡️&nbsp;Run it&nbsp;
+        <code>$ spellbook your-favourite-script</code>
+      </div>
+    </div>
+    <div>Spellbook - The magic is happening here ✨🔮</div>
   </section>
 );
