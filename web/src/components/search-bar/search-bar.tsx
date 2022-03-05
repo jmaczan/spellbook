a9b112d606478602832d60f860e7e40e948fdae6
jmaczan <jedrzejpawel@maczan.pl>
2022-03-05 21:45:36 +0100

    Navigate to search tab after typing search query

--- web/src/components/search-bar/search-bar.tsx
+++ web/src/components/search-bar/search-bar.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Spell } from '../spell-item/types';
 import debounce from 'lodash.debounce';
 import './search-bar.scss';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export interface SearchBarProps {
   onChange: (value: Spell[]) => void;
@@ -10,14 +11,18 @@ export interface SearchBarProps {
 export const SearchBar = ({ onChange }: SearchBarProps) => {
   const [query, setQuery] = useState<string>('');
 
+  const navigate = useNavigate();
+  const location = useLocation();
+
   useEffect(() => {
+    !!query && location.pathname !== '/search' && navigate('/search');
     !!query &&
       fetch('http://localhost:8080/api/search?query=' + query)
         .then((response) => response.json())
         .then((response) => {
           onChange(response);
         });
-  }, [query, onChange]);
+  }, [query, onChange, navigate, location]);
 
   const searchForSpells = (text: ChangeEvent<HTMLInputElement>) => {
     setQuery(text?.target.value);
