adaa0474fb1a81b1beb473f9e8938139d393c843
jmaczan <jedrzejpawel@maczan.pl>
2022-03-05 22:02:48 +0100

    Not found search text

--- web/src/components/search/search.tsx
+++ web/src/components/search/search.tsx
@@ -8,13 +8,14 @@ interface SearchProps {
   spells: Spell[];
 }
 
-export const Search = ({ spells }: SearchProps) => {
+export const Search = ({ spells = [] }: SearchProps) => {
   return (
     <section>
       <div className='search__spells'>
         {spells.map((spell) => (
           <SpellItem key={spell.name} spell={spell} />
         ))}
+        {spells.length === 0 ? <div className='search__not-found'>No spells found for your query 📚</div> : null}
       </div>
     </section>
   );
