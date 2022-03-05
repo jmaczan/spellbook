6ab7d5b355839b3fe13a9a147a1ca8535be26a01
jmaczan <jedrzejpawel@maczan.pl>
2022-03-05 22:22:58 +0100

    Add secondary accent in logo

--- web/src/components/navbar/Navbar.tsx
+++ web/src/components/navbar/Navbar.tsx
@@ -14,7 +14,9 @@ export const Navbar = ({ query, onChange, setQuery }: NavbarProps) => (
   <nav className='navbar'>
     <div>
       <Link to='/'>
-        <h1 className='navbar__title'>🪄 Spellbook 📖</h1>
+        <h1 className='navbar__title'>
+          🪄 <span className='navbar__title--half'>Spell</span>book 📖
+        </h1>
       </Link>
       <div className='navbar__subtitle'>Bash and Powershell scripts registry</div>
     </div>
