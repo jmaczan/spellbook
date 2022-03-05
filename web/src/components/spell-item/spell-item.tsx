19728ef43ff95dcb4d2a5a97b97b808e7baea9ea
jmaczan <jedrzejpawel@maczan.pl>
2022-03-05 22:21:34 +0100

    Secondary accent text in spell item

--- web/src/components/spell-item/spell-item.tsx
+++ web/src/components/spell-item/spell-item.tsx
@@ -6,16 +6,29 @@ interface SpellItemProps {
   spell: Spell;
 }
 
+const Separator = () => <span className='spell-item__separator'> ∙ </span>;
+
 export const SpellItem = ({ spell }: SpellItemProps) => (
   <div className='spell-item'>
     <div className='spell-item__title'>
       {randomSpellIcon()} {spell.title}
     </div>{' '}
     <div className='spell-item__description'>{spell.description}</div>
-    {spell.example ? <code className='spell-item__example'>$ spellbook {spell.example}</code> : null}
+    {spell.example ? (
+      <code className='spell-item__example'>
+        <span className='spell-item__shell-prompt'>$</span> spellbook {spell.example}
+      </code>
+    ) : null}
     <div className='spell-item__bottom'>
-      {spell.license ? <span className='spell-item__license'>{spell.license} ∙ </span> : null}
-      Version {spell.version} ∙ by {spell.author}
+      {spell.license ? (
+        <span className='spell-item__license'>
+          {spell.license}
+          <Separator />
+        </span>
+      ) : null}
+      Version {spell.version}
+      <Separator />
+      by <span className='spell-item__author'>{spell.author}</span>
     </div>
   </div>
 );
