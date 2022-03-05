502277e947ca8068e7df44b599e1ba54b2eea313
jmaczan <jedrzejpawel@maczan.pl>
2022-03-05 23:12:58 +0100

    License page text adjustment

--- web/src/components/license/license.tsx
+++ web/src/components/license/license.tsx
@@ -5,7 +5,7 @@ import './license.scss';
 export const License = () => (
   <section className='license'>
     <div>Each script has its own license.</div>
-    <div>Make sure that you use scripts in line with licenses.</div>
+    <div>Make sure you use scripts in line with their licenses.</div>
     <div className='license__infrigement'>
       If you notice any license infrigement here, please{' '}
       <ExternalLink url='mailto:jedrzejpawel@maczan.pl'>contact me immediately</ExternalLink>
