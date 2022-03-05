6998b03112501ecfe7344d5b67b8d26d23809231
jmaczan <jedrzejpawel@maczan.pl>
2022-03-05 12:29:51 +0100

    Add basic react routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/header/Header';
+import { Home } from './components/home/home';
 import { Install } from './components/install/Install';
 import { Search } from './components/search/Search';
 
@@ -8,8 +10,27 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <Install />
-      <Search />
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to='/'>Spellbook</Link>
+            </li>
+            <li>
+              <Link to='/install'>Install</Link>
+            </li>
+            <li>
+              <Link to='/search'>Search</Link>
+            </li>
+          </ul>
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/install' element={<Install />} />
+            <Route path='/search' element={<Search />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ web/src/components/home/home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Home = () => (
+  <section>
+    <div>The magic is happening here ✨🔮</div>
+  </section>
+);
