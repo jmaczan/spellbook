cea41ce8d321b43b431633437434926baeb680fc
jmaczan <jedrzejpawel@maczan.pl>
2022-03-05 14:49:12 +0100

    Howto links styling

--- web/src/components/home/home.tsx
+++ web/src/components/home/home.tsx
@@ -6,15 +6,21 @@ export const Home = () => (
   <section>
     <div className='home__howto'>
       <div className='howto__step'>
-        <Link to='/install'>Install</Link>&nbsp;Spellbook
+        <Link className='howto__link' to='/install'>
+          Install
+        </Link>
+        &nbsp;Spellbook
       </div>
       <div className='howto__step__icon'>➡️</div>
       <div className='howto__step'>
-        <Link to='/search'>Find</Link>&nbsp;your favourite script
+        <Link className='howto__link' to='/search'>
+          Find
+        </Link>
+        &nbsp;your favourite script
       </div>
       <div className='howto__step__icon'>➡️</div>
       <div className='howto__step'>
-        Speak the spell&nbsp;
+        Speak the spell&nbsp;&nbsp;
         <code>$ spellbook your-favourite-script</code>
       </div>
     </div>
