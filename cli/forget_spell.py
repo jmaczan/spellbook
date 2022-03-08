c8a262ef436df54b3313bb0c911e6530c0df4471
jmaczan <jedrzejpawel@maczan.pl>
2022-03-08 21:16:01 +0100

    Add forget validation

--- cli/forget_spell.py
+++ cli/forget_spell.py
@@ -3,7 +3,14 @@ from constants import spellbook_path
 
 
 def forget_spell(spell_name):
+    validate(spell_name)
+    validate(spellbook_path)
     non_empty_spellbook_path = spellbook_path or '/somenonsepathjusttonotbeempty$#!@#4123$()#@_'
     non_empty_spell_name = spell_name or '/somenonsepathjusttonotbeempty$#!@#4123$()#@_'
     system('rm -rf '+non_empty_spellbook_path+'/'+non_empty_spell_name)
     print("You forgot spell %s" % non_empty_spell_name)
+
+
+def validate(text):
+    if (text is None or text.strip() is '' or text.strip() is '/'):
+        print("Invalid configuration")
