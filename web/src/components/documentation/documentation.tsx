50dd80a6eb604a6570a132bea66d0b130461b3a4
jmaczan <jedrzejpawel@maczan.pl>
2022-03-10 22:48:17 +0100

    Add basic documentation

--- web/src/components/documentation/documentation.tsx
+++ web/src/components/documentation/documentation.tsx
@@ -1,4 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './documentation.scss';
 
-export const Documentation = () => <section className='documentation'>It just works</section>;
+export const Documentation = () => (
+  <section className='documentation'>
+    <div>Documentation</div>
+    <div className='documentation__item'>
+      <Link to='/install'>Install</Link> spellbook cli app
+    </div>
+    <div className='documentation__item'>
+      <Link to='/search'>Browse the spellbook</Link> and find a useful script
+    </div>
+    <div className='documentation__item'>Use cli app to run scripts</div>
+    <div className='documentation__item'>Currently available commands:</div>
+    <ol>
+      <li className='documentation__item'>
+        Show installed scripts - <code>spell list</code>
+      </li>
+      <li className='documentation__item'>
+        Add a script from repository - <code>spell learn</code>
+      </li>
+      <li className='documentation__item'>
+        Run a script - <code>spell &lt;script name&gt; [parameters]</code>
+      </li>
+      <li className='documentation__item'>
+        Remove a script - <code>spell forget &lt;script name&gt;</code>
+      </li>
+    </ol>
+  </section>
+);
