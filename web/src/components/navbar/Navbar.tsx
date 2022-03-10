0e53df26155c364aee4029926a2096ccdfee2893
jmaczan <jedrzejpawel@maczan.pl>
2022-03-10 20:16:00 +0100

    Basic mobile navbar responsiveness

--- web/src/components/navbar/Navbar.tsx
+++ web/src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEventHandler, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { SearchBar } from '../search-bar/search-bar';
 import { Spell } from '../spell-item/types';
@@ -10,45 +10,71 @@ interface NavbarProps {
   setQuery: (value: string) => void;
 }
 
-export const Navbar = ({ query, onChange, setQuery }: NavbarProps) => (
-  <nav className='navbar'>
-    <div className='navbar__title-with-search'>
-      <div>
-        <Link to='/'>
-          <h1 className='navbar__title'>
-            {' '}
-            🪄 <span className='navbar__title--secondary-half'>Spell</span>
-            <span className='navbar__title--primary-half'>book</span> 📖
-          </h1>
-        </Link>
-        <div className='navbar__subtitle'>Shell and Powershell scripts registry</div>
-      </div>
-      <SearchBar onChange={onChange} query={query} setQuery={setQuery} />
-      <div className='navbar__burger-menu'>
+export const Navbar = ({ query, onChange, setQuery }: NavbarProps) => {
+  const [isMenuToggled, toggleMenu] = useState(false);
+
+  const toggleMenuHandler = () => toggleMenu(!isMenuToggled);
+
+  const closeMenu = () => toggleMenu(false);
+
+  return (
+    <nav className='navbar'>
+      <div className='navbar__title-with-search'>
         <div>
-          <svg viewBox='0 0 100 80' width='40' height='40'>
-            <rect width='70' height='7' fill='#FDE74C'></rect>
-            <rect y='25' width='70' height='7' fill='#FDE74C'></rect>
-            <rect y='50' width='70' height='7' fill='#FDE74C'></rect>
-          </svg>
+          <Link to='/'>
+            <h1 className='navbar__title'>
+              <span className='navbar__title__icon'>🪄 </span>
+              <span className='navbar__title--secondary-half'>Spell</span>
+              <span className='navbar__title--primary-half'>book</span>
+              <span className='navbar__title__icon'> 📖</span>
+            </h1>
+          </Link>
+          <div className='navbar__subtitle'>Shell and Powershell scripts registry</div>
         </div>
+        <div className='navbar__burger-menu' onClick={toggleMenuHandler}>
+          <div>
+            <svg viewBox='0 -25 25 100' width='35' height='30'>
+              <rect width='70' height='7' fill='#FDE74C'></rect>
+              <rect y='25' width='70' height='7' fill='#FDE74C'></rect>
+              <rect y='50' width='70' height='7' fill='#FDE74C'></rect>
+            </svg>
+          </div>
+        </div>
+        <SearchBar onChange={onChange} query={query} setQuery={setQuery} />
       </div>
-    </div>
-    <div>
-      <ul className='navbar__items'>
-        <Link to='/install'>
-          <li className='navbar__item'>Install</li>
-        </Link>
-        <Link to='/search'>
-          <li className='navbar__item'>Search</li>
-        </Link>
-        <Link to='/documentation'>
-          <li className='navbar__item'>Documentation</li>
-        </Link>
-        <Link to='/add'>
-          <li className='navbar__item'>📤&nbsp;&nbsp;Add</li>
-        </Link>
-      </ul>
-    </div>
-  </nav>
-);
+      <div className='navbar__subtitle navbar__subtitle--mobile'>Shell and Powershell scripts registry</div>
+      <div className='navbar__items-container'>
+        <ul className='navbar__items'>
+          <Link to='/install'>
+            <li className='navbar__item'>Install</li>
+          </Link>
+          <Link to='/search'>
+            <li className='navbar__item'>Search</li>
+          </Link>
+          <Link to='/documentation'>
+            <li className='navbar__item'>Documentation</li>
+          </Link>
+          <Link to='/add'>
+            <li className='navbar__item'>📤&nbsp;&nbsp;Add</li>
+          </Link>
+        </ul>
+      </div>
+      {isMenuToggled ? (
+        <ul className='navbar__items navbar__items-container--mobile'>
+          <Link to='/install' onClick={closeMenu}>
+            <li className='navbar__item'>Install</li>
+          </Link>
+          <Link to='/search' onClick={closeMenu}>
+            <li className='navbar__item'>Search</li>
+          </Link>
+          <Link to='/documentation' onClick={closeMenu}>
+            <li className='navbar__item'>Documentation</li>
+          </Link>
+          <Link to='/add' onClick={closeMenu}>
+            <li className='navbar__item'>📤&nbsp;&nbsp;Add</li>
+          </Link>
+        </ul>
+      ) : null}
+    </nav>
+  );
+};
