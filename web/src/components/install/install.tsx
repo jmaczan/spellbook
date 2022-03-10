26f62bdc3ccab6ad8aaea7da3033db8b53215a4d
jmaczan <jedrzejpawel@maczan.pl>
2022-03-10 22:38:46 +0100

    Install tips

--- web/src/components/install/install.tsx
+++ web/src/components/install/install.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { ExternalLink } from '../external-link/external-link';
+import './install.scss';
 
 export const Install = () => (
-  <section>
+  <section className='install'>
     <h1>Install</h1>
-    <div>Download binary from link</div>
+    <div>
+      The only step is to download a binary from{' '}
+      <ExternalLink url='https://github.com/jmaczan/spellbook/releases/download/v0.0.1/spell'> here</ExternalLink>
+    </div>
+    <div>Once you download the program, run it in your favourite terminal</div>
+    <div></div>
+    <div className='install--alternative'>
+      Alternatively, you can build it from the source. The repository is{' '}
+      <ExternalLink url='https://github.com/jmaczan/spellbook/tree/main/cli'> here</ExternalLink>
+    </div>
   </section>
 );
