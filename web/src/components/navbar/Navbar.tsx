dc818e52a54a702ac5c40c3ebd5dd3f62cf7e8c8
jmaczan <jedrzejpawel@maczan.pl>
2022-03-10 19:48:26 +0100

    Navbar responsiveness - WIP

--- web/src/components/navbar/Navbar.tsx
+++ web/src/components/navbar/Navbar.tsx
@@ -12,17 +12,28 @@ interface NavbarProps {
 
 export const Navbar = ({ query, onChange, setQuery }: NavbarProps) => (
   <nav className='navbar'>
-    <div>
-      <Link to='/'>
-        <h1 className='navbar__title'>
-          {' '}
-          🪄 <span className='navbar__title--secondary-half'>Spell</span>
-          <span className='navbar__title--primary-half'>book</span> 📖
-        </h1>
-      </Link>
-      <div className='navbar__subtitle'>Shell and Powershell scripts registry</div>
+    <div className='navbar__title-with-search'>
+      <div>
+        <Link to='/'>
+          <h1 className='navbar__title'>
+            {' '}
+            🪄 <span className='navbar__title--secondary-half'>Spell</span>
+            <span className='navbar__title--primary-half'>book</span> 📖
+          </h1>
+        </Link>
+        <div className='navbar__subtitle'>Shell and Powershell scripts registry</div>
+      </div>
+      <SearchBar onChange={onChange} query={query} setQuery={setQuery} />
+      <div className='navbar__burger-menu'>
+        <div>
+          <svg viewBox='0 0 100 80' width='40' height='40'>
+            <rect width='70' height='7' fill='#FDE74C'></rect>
+            <rect y='25' width='70' height='7' fill='#FDE74C'></rect>
+            <rect y='50' width='70' height='7' fill='#FDE74C'></rect>
+          </svg>
+        </div>
+      </div>
     </div>
-    <SearchBar onChange={onChange} query={query} setQuery={setQuery} />
     <div>
       <ul className='navbar__items'>
         <Link to='/install'>
