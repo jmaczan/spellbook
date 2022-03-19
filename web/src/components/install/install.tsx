cd2a9444906b9d81c6d2b2bde65b2944829850db
jmaczan <jedrzejpawel@maczan.pl>
2022-03-19 15:06:33 +0100

    Install spacing

--- web/src/components/install/install.tsx
+++ web/src/components/install/install.tsx
@@ -9,30 +9,30 @@ export const Install = () => (
     <ExternalLink url='https://github.com/jmaczan/spellbook/releases/download/v0.0.1/spell'>
       <InstallCard icon='🐧' title='Linux' />
     </ExternalLink>
-    <div>
-      Make <code>spell</code> executable{' '}
-      <code>
-        <span className='spell-item__shell-prompt'>$</span> chmod +x spell
-      </code>
+    <div className='install__instruction'>
+      <div>
+        Make <code>spell</code> executable{' '}
+        <code>
+          <span className='spell-item__shell-prompt'>$</span> chmod +x spell
+        </code>
+      </div>
+      <div>
+        Add <code>spell</code> to <code>$PATH</code> variable{' '}
+        <code>
+          <span className='spell-item__shell-prompt'>$</span> echo &quot;export PATH=`pwd`:$PATH&quot; &gt;&gt;
+          ~/.bashrc | source ~/.bashrc
+        </code>{' '}
+        or put <code>spell</code> in folder that&apos;s already in <code>$PATH</code> variable{' '}
+        <code>
+          <span className='spell-item__shell-prompt'>$</span> sudo mv spell /usr/local/bin
+        </code>
+      </div>
+      <div>
+        Done! Ready to make the magic happen{' '}
+        <code className='spell-item__example'>
+          <span className='spell-item__shell-prompt'>$</span> spell repeat-sh 5 ls -no-clean
+        </code>
+      </div>
     </div>
-    <div>
-      Add <code>spell</code> to <code>$PATH</code> variable{' '}
-      <code>
-        <span className='spell-item__shell-prompt'>$</span> echo &quot;export PATH=`pwd`:$PATH&quot; &gt;&gt; ~/.bashrc
-        | source ~/.bashrc
-      </code>{' '}
-      or put <code>spell</code> in folder that&apos;s already in <code>$PATH</code> variable{' '}
-      <code>
-        <span className='spell-item__shell-prompt'>$</span> sudo mv spell /usr/local/bin
-      </code>
-    </div>
-    <div>
-      Done! Ready to make the magic happen{' '}
-      <code className='spell-item__example'>
-        <span className='spell-item__shell-prompt'>$</span> spell repeat-sh 5 ls -no-clean
-      </code>
-    </div>
-
-    <div></div>
   </section>
 );
