53ac41d76c5d544dac0f16d1af85aa9c85079d2d
Jędrzej Paweł Maczan <jedrzej@Event-Horizon.local>
2023-08-13 19:42:27 +0200

    OTP/Subscription

--- web/src/components/pricing/pricing.tsx
+++ web/src/components/pricing/pricing.tsx
@@ -5,6 +5,7 @@ import './pricing.scss';
 export const Pricing = () => (
   <section className='pricing'>
     <div>✨ Free for personal / non-commercial use</div>
-    <div>🔮 To use it in a company / for commercial use, <ExternalLink url={process.env.REACT_APP_PAYMENT_URL || '#'}>pay $10 per year per user</ExternalLink></div>
+    <div>🔮 To use it in a company / for commercial use, <ExternalLink url={process.env.REACT_APP_PAYMENT_URL_SUBSCRIPTION || '#'}>pay $10 per year per user</ExternalLink></div>
+    <div><ExternalLink url={process.env.REACT_APP_PAYMENT_URL_SUBSCRIPTION || '#'}>Click here</ExternalLink> to buy a yearly subscription or <ExternalLink url={process.env.REACT_APP_PAYMENT_URL_OTP || '#'}>click here</ExternalLink> for one-time payment for one year license without renewing subscription</div>
   </section>
 );
